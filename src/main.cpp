b2d3f5fd821b58ef56de64e96b0e116d2b612164
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2024-10-03 12:53:09 -0700

    fullscreen-sdl

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,9 @@ int main(int argc, char* argv[]) {
             std::string patchFile = argv[i + 1];
             MemoryPatcher::patchFile = patchFile;
         }
+        if (curArg == "-f") || (curArg == "--fullscreen") {
+            bool isFullscreen = true;
+        }
     }
 
     Core::Emulator emulator;
